3a660dbfa48f5cac35d485292a8e4c669c52883d
DAs240 <53143620+DAs240@users.noreply.github.com>
2020-11-17 22:30:30 +0600

    homework1

---
+++ Homework Submission/Homework 1/1.asm
@@ -0,0 +1,17 @@
+
+; You may customize this and other start-up templates; 
+; The location of this template is c:\emu8086\inc\0_com_template.txt
+
+org 100h
+
+MOV AX, 04
+ADD AX, 02 
+ADD AX, 01  
+    
+MOV CX, AX  
+
+ret
+
+
+
+
---
+++ Homework Submission/Homework 1/2.asm
@@ -0,0 +1,21 @@
+
+; You may customize this and other start-up templates; 
+; The location of this template is c:\emu8086\inc\0_com_template.txt
+
+org 100h
+
+
+ADD AX, 02 
+ADD AX, 04 
+ADD AX, 06
+ADD AX, 08
+ADD AX, 10 
+
+MOV CX, AX
+ret
+
+
+
+
+            
+   
